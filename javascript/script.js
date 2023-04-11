c1388e04baf092dffd899dc5f809a5fd8f8ffe0c
abhinav1836 <129741919+abhinav1836@users.noreply.github.com>
2023-04-11 13:05:31 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -25,3 +25,13 @@ hamburger.addEventListener("click",()=>{
   hamburger.classList.toggle ("active-ham");
   list.classList.toggle ("active-ham");
 })
+
+// ?add event listener to all links inside hamburger menu
+const links = document.querySelectorAll(".list-items a");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    // close hamburger menu
+    hamburger.classList.remove("active-ham");
+    list.classList.remove("active-ham");
+  });
+});
